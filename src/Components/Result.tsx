33879d032233254d2e78fc4e141cd1dc772187fb
KizuraAmberfang <diana.blackhurst@gmail.com>
2021-11-06 00:28:24 +0100

    prima prova

---
+++ src/Components/Result.tsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react'
+
+function Result() {
+    const [data, setData] = useState({
+        kind: 'default',
+        items: [], 
+        totalItems: 0
+    });
+
+    useEffect(() => {
+        if (data.kind === 'default')
+            getData()
+    })
+
+    const getData = async () => {
+        const data = await fetch('https://www.googleapis.com/books/v1/volumes?q=flowers', { method: 'GET' }).then((res) => res.json())
+        setData(data);
+    }
+
+    return (
+        <div>
+            {data.totalItems}
+        </div>
+        );
+}
+
+export default Result;
