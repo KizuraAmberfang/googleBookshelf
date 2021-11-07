a556a8e7c3624bc4e03b6cd8abce38a5d0d7bc56
KizuraAmberfang <63409735+KizuraAmberfang@users.noreply.github.com>
2021-11-07 01:49:29 +0100

    tabella creata!

--- src/Components/Row.tsx
+++ src/Components/Row.tsx
@@ -1,11 +1,22 @@
 function Rows(props: any) {
   const {
     volumeInfo: { title },
-    volumeInfo: { subtitle }
+    volumeInfo: { subtitle },
+    volumeInfo: { authors },
+    volumeInfo: { description }
   } = props.data;
   return (
-    <div className="post">
-      <h1>{title}</h1> <h5>{subtitle}</h5>
+    <div>
+      <div className="row">
+        <div className="col-2">
+          <div>{title}</div>
+          <div>
+            <small>{subtitle}</small>
+          </div>
+        </div>
+        <div className="col-2">{authors}</div>
+        <div className="col-8">{description}</div>
+      </div>
     </div>
   );
 }
